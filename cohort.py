04781fb0ee4bd9e23ed87c2b8f92c61fce3d274d
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-07-17 17:37:00 -0500

    wip

--- cohort.py
+++ cohort.py
@@ -3,3 +3,9 @@ class Cohort:
     self.name = name
     self.students = []
     self.instructors = []
+
+  def add_students(self, student):
+    self.students.append(student)
+
+  def add_instructors(self, instructor):
+    self.instructors.append(instructor)
--- student.py
+++ student.py
@@ -1,7 +1,16 @@
 class Student:
-  def __init__(self, first_name, last_name, slack_handle, cohort)
+  def __init__(self, first_name, last_name, slack_handle):
     self.first_name = first_name
     self.last_name = last_name
     self.slack_handle = slack_handle
-    self.cohort = cohort
-    self.exercises = []
+    self.cohort = []
+    self.exercise = []
+
+  def add_cohort(self, cohort):
+    self.cohort.append(cohort)
+  
+  def add_exercise(self, exercise):
+    self.cohort.append(exercise)
+
+  def __str__(self):
+    return f"{self.first_name} is in {self.cohort}"
