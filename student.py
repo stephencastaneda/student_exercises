f29e012ec9bc26c80919bce06eab669b138e00f4
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-07-20 12:45:19 -0500

    finished part 2

--- student.py
+++ student.py
@@ -1,8 +1,7 @@
-class Student:
-  def __init__(self, first_name, last_name, slack_handle):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.slack_handle = slack_handle
+from NSSperson import NSSPerson
+
+class Student(NSSPerson):
+  def __init__(self):
     self.cohort = []
     self.exercise = []
 
